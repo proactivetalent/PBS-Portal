02abe1bf6498b9a89f9e5260cba3d0ec0b5c2ce9
ahsinali-17 <aajoker17@gmail.com>
2025-07-22 12:33:47 +0500

    gtm scripts integration

--- resources/views/portal/master.blade.php
+++ resources/views/portal/master.blade.php
@@ -3,20 +3,14 @@
 @section('meta_tags')
     <title>@yield('title', 'PBS Portal')</title>
     <meta name="description" content="@yield('meta_description', 'PBS Portal for property management, alerts, and more.')">
-    
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-54XWJQ7ZSL"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
 
-        function gtag() {
-            dataLayer.push(arguments);
-        }
-
-        gtag('js', new Date());
-
-        gtag('config', 'G-54XWJQ7ZSL');
-    </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-563Q5G8V');</script>
+    <!-- End Google Tag Manager -->
 @endsection
 
 @section('css')
@@ -104,6 +98,11 @@
             }
         }
     </style>
+
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-563Q5G8V"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     
     <script>
         document.addEventListener('DOMContentLoaded', function() {
@@ -263,6 +262,5 @@
         });
     </script>
 @append
-
 
 @include('portal.partials.footer')
